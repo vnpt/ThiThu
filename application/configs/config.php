3f9a50aefd5bcc4aa4007982dad62e9ded696a8e
Phạm Minh Đạt <pmdat91@gmail.com>
2012-10-29 23:53:38 +0700

    config

--- application/configs/config.php
+++ application/configs/config.php
@@ -4,4 +4,17 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+ini_set("display_errors", true);
+ini_set ('magic_quotes_gpc', 0);
+date_default_timezone_set("Asia/Ho_Chi_Minh");
+define("DB_DSN", "mysql:host=localhost;dbname=tsbzamzy_dbbanhang");
+define("DB_USERNAME", "tsbzamzy_bh");
+define("DB_PASSWORD", "dbbanhang12345");
+define("NAME_WEBSITE", "Thi thử đại học");
+define("ROOT_PATH", "http://localhost/ThiThuDH/");
+define("ROOT_PATH_2", "http://localhost/ThiThuDH");
+define("NUMBER_ARTICLE_PER_PAGE", 10);
+define("CLASS_PATH", APPLICATION_PATH . "/models/basic");
+require (CLASS_PATH . "/User.php");
+
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+    define("APPLICATION_PATH", "application");
+    require (APPLICATION_PATH . "/configs/config.php");
+    require (APPLICATION_PATH . "/controllers/index/logic/IndexController.php");
+?>
